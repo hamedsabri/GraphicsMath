f5b1df781708c0ef7967b565d03f9af3e13adfdd
moddyz <moddy3d@gmail.com>
2020-05-13 18:27:13 +1200

    Removed export header.

--- lib/gm/api.h
+++ lib/gm/api.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <gm/platform/export.h>
-
-/// Symbol exporting.
-#define GM_API GM_EXPORT
-
-/// Namespaces.
+/// Pre-processor definitions for namespaces.
 #define GM_NAMESPACE gm
 #define GM_NAMESPACE_USING using namespace GM_NAMESPACE;
 #define GM_NAMESPACE_BEGIN namespace GM_NAMESPACE {
