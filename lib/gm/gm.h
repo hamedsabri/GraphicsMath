11c16497f5df2749d1e19ee2cf8ad127c6e9e9bb
moddyz <moddy3d@gmail.com>
2020-05-13 21:46:48 +1200

    Updated gm.h docstring.

--- lib/gm/gm.h
+++ lib/gm/gm.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/// \file gm/gm.h
+///
+/// GraphicsMath: C++ header-only computer graphics math library.
+
 /// Pre-processor definitions for namespaces.
 #define GM_NS gm
 #define GM_NS_USING using namespace GM_NS;
