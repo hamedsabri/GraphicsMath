09fb14a899b04509c07f8c57fab517189d07d41f
moddyz <moddy3d@gmail.com>
2020-05-15 22:33:33 +1200

    Added doc for GM_HOST_DEVICE.

--- lib/gm/base/hostdevice.h
+++ lib/gm/base/hostdevice.h
@@ -1,9 +1,10 @@
 #pragma once
 
 /// \file hostdevice.h
-///
-/// Definitions to support functions to be callable from CUDA device code.
 
+/// \def GM_HOST_DEVICE
+///
+/// Definition to allow functions to be utilized from both host and CUDA device code.
 #if defined( GM_CUDA_SUPPORT )
 #define GM_HOST_DEVICE __host__ __device__
 #else
