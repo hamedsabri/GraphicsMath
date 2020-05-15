bccc961d8d472820b8ca36d4f3f0e4420dfd312c
moddyz <moddy3d@gmail.com>
2020-05-15 21:53:35 +1200

    Improved assertion tooling with documentation and file & line contextual info.

--- lib/gm/tool/assert.h
+++ lib/gm/tool/assert.h
@@ -12,37 +12,51 @@
 /// In Debug builds, GM_ASSERT macro will print the stacktrace when the input expression evaluates to false.
 /// In Release builds, the GM_ASSERT macro will be pre-processed away in Release builds.
 
+/// \def GM_ASSERT( expr )
+/// Asserts that the expression \p expr is \em true in debug builds. If \p expr evalutes \em false,
+/// an error message will be printed with contextual information such as the source code location.
+///
+/// In release builds, this is compiled out.
 #ifdef GM_DEBUG
 #define GM_ASSERT( expr )                                                                                              \
     if ( !( expr ) )                                                                                                   \
     {                                                                                                                  \
-        gm::Assert( #expr );                                                                                           \
-    }
-#define GM_ASSERT_MSG( expr, format, ... )                                                                             \
-    if ( !( expr ) )                                                                                                   \
-    {                                                                                                                  \
-        gm::Assert( #expr, format, ##__VA_ARGS__ );                                                                    \
+        gm::_Assert( #expr, __FILE__, __LINE__ );                                                                      \
     }
 #else
 #define GM_ASSERT( expr, ... ) void()
+#endif
+
+/// \def GM_ASSERT_MSG( expr, format, ... )
+/// Similar to \ref GM_ASSERT, with the addition of a formatted message when the expression \p expr fails to evaluate
+/// in debug builds.
+#ifdef GM_DEBUG
+#define GM_ASSERT_MSG( expr, format, ... )                                                                             \
+    if ( !( expr ) )                                                                                                   \
+    {                                                                                                                  \
+        gm::_Assert( #expr, format, __FILE__, __LINE__, ##__VA_ARGS__ );                                               \
+    }
+#else
 #define GM_ASSERT_MSG( expr, format, ... ) void()
 #endif
 
 GM_NS_BEGIN
 
-void Assert( const char* i_expression )
+/// Not intended to be used directly, \ref GM_ASSERT instead.
+void _Assert( const char* i_expression, const char* i_file, size_t i_line )
 {
-    fprintf( stderr, "Assert failed for expression: %s\n", i_expression );
+    fprintf( stderr, "Assertion failed for expression: %s, at %s:%lu\n", i_expression, i_file, i_line );
 }
 
-void Assert( const char* i_expression, const char* i_format, ... )
+/// Not intended to be used directly, \ref GM_ASSERT_MSG instead.
+void _Assert( const char* i_expression, const char* i_file, size_t i_line, const char* i_format, ... )
 {
     char    buffer[ 256 ];
     va_list args;
     va_start( args, i_format );
     vsprintf( buffer, i_format, args );
     va_end( args );
-    fprintf( stderr, "Assert failed for expression: %s, %s\n", i_expression, buffer );
+    fprintf( stderr, "Assertion failed for expression: %s, at %s:%lu, %s\n", i_expression, i_file, i_line, buffer );
 }
 
 GM_NS_END
