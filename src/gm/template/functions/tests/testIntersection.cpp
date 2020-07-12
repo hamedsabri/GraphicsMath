f0071203d3e670f25e58c689b4a54acde97e1c19
moddyz <moddy3d@gmail.com>
2020-07-12 13:39:17 +1200

    Simplified template/functions/tests/testIntersection.cpp

--- src/gm/template/functions/tests/testIntersection.cpp
+++ src/gm/template/functions/tests/testIntersection.cpp
@@ -5,26 +5,27 @@
 {% import "types/typeUtils.h" as typeUtils %}
 
 {% for interface in function.interfaces %}
+{% set rangeType = interface.ArgType("lhs") %}
 TEST_CASE( "{{ function.name }}_{{ interface.ArgClass("lhs") }}" )
 {
     // Overlaps.
     CHECK( gm::{{ function.name }}(
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -2, 2 ) -}},
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -3, -1 ) -}}
-    ) == {{- typeUtils.GenRange( interface.ArgType("lhs"), -2, -1 ) -}} );
+        {{- typeUtils.GenRange( rangeType, -2, 2 ) -}},
+        {{- typeUtils.GenRange( rangeType, -3, -1 ) -}}
+    ) == {{- typeUtils.GenRange( rangeType, -2, -1 ) -}} );
     CHECK( gm::{{ function.name }}(
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -2, 2 ) -}},
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -1, 1 ) -}}
-    ) == {{- typeUtils.GenRange( interface.ArgType("lhs"), -1, 1 ) -}} );
+        {{- typeUtils.GenRange( rangeType, -2, 2 ) -}},
+        {{- typeUtils.GenRange( rangeType, -1, 1 ) -}}
+    ) == {{- typeUtils.GenRange( rangeType, -1, 1 ) -}} );
 
     // Empty.
     CHECK( gm::{{ function.name }}(
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -2, 2 ) -}},
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -5, -3 ) -}}
+        {{- typeUtils.GenRange( rangeType, -2, 2 ) -}},
+        {{- typeUtils.GenRange( rangeType, -5, -3 ) -}}
     ).IsEmpty() );
     CHECK( gm::{{ function.name }}(
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), -2, 2 ) -}},
-        {{- typeUtils.GenRange( interface.ArgType("lhs"), 1, -1 ) -}}
+        {{- typeUtils.GenRange( rangeType, -2, 2 ) -}},
+        {{- typeUtils.GenRange( rangeType, 1, -1 ) -}}
     ).IsEmpty() );
 }
 {% endfor %}
