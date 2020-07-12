e1cf1aa4a79c886b6197e5589ba1938c011bce86
moddyz <moddy3d@gmail.com>
2020-07-12 13:27:06 +1200

    Simplified template/functions/intersection.h

--- src/gm/template/functions/intersection.h
+++ src/gm/template/functions/intersection.h
@@ -4,7 +4,7 @@
 {%- block fileDoc -%}
 /// Range intersection.
 ///
-/// Computes the intersection, or overlapping range from two input ranges.
+/// Computes the intersection, or overlap, of two input ranges.
 {%- endblock %}
 
 {% block includes %}
@@ -16,16 +16,21 @@
 
 {% block body %}
 {% for interface in function.interfaces %}
-/// Compute the intersecting range of \p {{ interface.ArgName("lhs") }} and \p {{ interface.ArgName("rhs") }}.
+{% set category = interface.ArgType("lhs").CATEGORY %}
+{% set lhs = interface.ArgName("lhs") %}
+{% set rhs = interface.ArgName("rhs") %}
+/// Compute the intersecting {{ category }} of \p {{ lhs }} and \p {{ rhs }}.
 ///
-/// \param i_range The range to compare against.
+/// \param {{ lhs }} The first {{ category }}.
+/// \param {{ rhs }} The second {{ category }}.
 ///
-/// \return The intersecting range.  An empty range will be returned if there is no overlap.
+/// \return The {{ category }} intersection.  An empty {{ category }} will be returned if there is no overlap.
+/// \sa \ref {{ interface.ArgClass("lhs") }}::IsEmpty()
 {{- functionUtils.signature(function, interface) -}}
 {
     return {{ interface.returnType.className }}(
-        Max( {{ interface.ArgName("lhs") }}.Min(), {{ interface.ArgName("rhs") }}.Min() ),
-        Min( {{ interface.ArgName("lhs") }}.Max(), {{ interface.ArgName("rhs") }}.Max() )
+        Max( {{ lhs }}.Min(), {{ rhs }}.Min() ),
+        Min( {{ lhs }}.Max(), {{ rhs }}.Max() )
     );
 }
 {% endfor %}
