28522bb91975795cda14aa293e36489a370891cf
moddyz <moddy3d@gmail.com>
2020-07-14 21:21:38 +1200

    Renamed GenRisingValue -> GenArithmeticSeq

--- src/gm/template/functions/tests/testDotProduct.cpp
+++ src/gm/template/functions/tests/testDotProduct.cpp
@@ -10,8 +10,8 @@
 {% set namespacedVectorClass = vectorType.namespacedClassName %}
 TEST_CASE( "{{ function.name }}_{{ vectorClass }}" )
 {
-    {{ namespacedVectorClass }} {{ vectorType.varName }}A = {{ typeUtils.GenRisingValue(vectorType, 1) }};
-    {{ namespacedVectorClass }} {{ vectorType.varName }}B = {{ typeUtils.GenRisingValue(vectorType, 5) }};
+    {{ namespacedVectorClass }} {{ vectorType.varName }}A = {{ typeUtils.GenArithmeticSeq(vectorType, 1) }};
+    {{ namespacedVectorClass }} {{ vectorType.varName }}B = {{ typeUtils.GenArithmeticSeq(vectorType, 5) }};
     CHECK( gm::{{ function.name }}( {{ vectorType.varName }}A, {{ vectorType.varName }}B )
 {% if vectorType.elementSize == 2 -%}
            == {{ vectorType.CppValue(5) }}
