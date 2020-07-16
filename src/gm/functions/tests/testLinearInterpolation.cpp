050719fdbd88f128a62c33746d7ecc5ec0730eb6
moddyz <moddy3d@gmail.com>
2020-07-16 21:58:19 +1200

    Simplified LinearInterpolation test template code (greatly).

--- src/gm/functions/tests/testLinearInterpolation.cpp
+++ src/gm/functions/tests/testLinearInterpolation.cpp
@@ -8,107 +8,97 @@
 
 TEST_CASE( "LinearInterpolation_float" )
 {
-    float floatValA    = 3.0f;
-    float floatValB    = 1.0f;
-    float floatValLerp = gm::LinearInterpolation( floatValA, floatValB, 0.5f );
-    CHECK( floatValLerp == Approx( 2.0f ) );
+    float source = 0.0f;
+    float target = 5.0f;
+    float result = gm::LinearInterpolation( source,
+                                            target,
+                                            /* weight */ 0.5 );
+
+    CHECK( result == 2.5f );
 }
 
 TEST_CASE( "LinearInterpolation_Mat3f" )
 {
-    gm::Mat3f matrixA( 0.0f, 2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f );
-    gm::Mat3f matrixB( 0.0f, 4.0f, 8.0f, 12.0f, 16.0f, 20.0f, 24.0f, 28.0f, 32.0f );
-    gm::Mat3f matrixLerp = gm::LinearInterpolation( matrixA, matrixB, 0.5f );
-    CHECK( matrixLerp[ 0 ] == Approx( 0.0f ) );
-    CHECK( matrixLerp[ 1 ] == Approx( 3.0f ) );
-    CHECK( matrixLerp[ 2 ] == Approx( 6.0f ) );
-    CHECK( matrixLerp[ 3 ] == Approx( 9.0f ) );
-    CHECK( matrixLerp[ 4 ] == Approx( 12.0f ) );
-    CHECK( matrixLerp[ 5 ] == Approx( 15.0f ) );
-    CHECK( matrixLerp[ 6 ] == Approx( 18.0f ) );
-    CHECK( matrixLerp[ 7 ] == Approx( 21.0f ) );
-    CHECK( matrixLerp[ 8 ] == Approx( 24.0f ) );
+    gm::Mat3f source = gm::Mat3f( 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f );
+    gm::Mat3f target = gm::Mat3f( 0.0f, 5.0f, 10.0f, 15.0f, 20.0f, 25.0f, 30.0f, 35.0f, 40.0f );
+    gm::Mat3f result = gm::LinearInterpolation( source,
+                                                target,
+                                                /* weight */ 0.5 );
+
+    CHECK( result == gm::Mat3f( 0.0f, 2.5f, 5.0f, 7.5f, 10.0f, 12.5f, 15.0f, 17.5f, 20.0f ) );
 }
 
 TEST_CASE( "LinearInterpolation_Mat4f" )
 {
-    gm::Mat4f matrixA( 0.0f,
-                       2.0f,
-                       4.0f,
-                       6.0f,
-                       8.0f,
-                       10.0f,
-                       12.0f,
-                       14.0f,
-                       16.0f,
-                       18.0f,
-                       20.0f,
-                       22.0f,
-                       24.0f,
-                       26.0f,
-                       28.0f,
-                       30.0f );
-    gm::Mat4f matrixB( 0.0f,
-                       4.0f,
-                       8.0f,
-                       12.0f,
-                       16.0f,
-                       20.0f,
-                       24.0f,
-                       28.0f,
-                       32.0f,
-                       36.0f,
-                       40.0f,
-                       44.0f,
-                       48.0f,
-                       52.0f,
-                       56.0f,
-                       60.0f );
-    gm::Mat4f matrixLerp = gm::LinearInterpolation( matrixA, matrixB, 0.5f );
-    CHECK( matrixLerp[ 0 ] == Approx( 0.0f ) );
-    CHECK( matrixLerp[ 1 ] == Approx( 3.0f ) );
-    CHECK( matrixLerp[ 2 ] == Approx( 6.0f ) );
-    CHECK( matrixLerp[ 3 ] == Approx( 9.0f ) );
-    CHECK( matrixLerp[ 4 ] == Approx( 12.0f ) );
-    CHECK( matrixLerp[ 5 ] == Approx( 15.0f ) );
-    CHECK( matrixLerp[ 6 ] == Approx( 18.0f ) );
-    CHECK( matrixLerp[ 7 ] == Approx( 21.0f ) );
-    CHECK( matrixLerp[ 8 ] == Approx( 24.0f ) );
-    CHECK( matrixLerp[ 9 ] == Approx( 27.0f ) );
-    CHECK( matrixLerp[ 10 ] == Approx( 30.0f ) );
-    CHECK( matrixLerp[ 11 ] == Approx( 33.0f ) );
-    CHECK( matrixLerp[ 12 ] == Approx( 36.0f ) );
-    CHECK( matrixLerp[ 13 ] == Approx( 39.0f ) );
-    CHECK( matrixLerp[ 14 ] == Approx( 42.0f ) );
-    CHECK( matrixLerp[ 15 ] == Approx( 45.0f ) );
+    gm::Mat4f source =
+        gm::Mat4f( 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f );
+    gm::Mat4f target = gm::Mat4f( 0.0f,
+                                  5.0f,
+                                  10.0f,
+                                  15.0f,
+                                  20.0f,
+                                  25.0f,
+                                  30.0f,
+                                  35.0f,
+                                  40.0f,
+                                  45.0f,
+                                  50.0f,
+                                  55.0f,
+                                  60.0f,
+                                  65.0f,
+                                  70.0f,
+                                  75.0f );
+    gm::Mat4f result = gm::LinearInterpolation( source,
+                                                target,
+                                                /* weight */ 0.5 );
+
+    CHECK( result == gm::Mat4f( 0.0f,
+                                2.5f,
+                                5.0f,
+                                7.5f,
+                                10.0f,
+                                12.5f,
+                                15.0f,
+                                17.5f,
+                                20.0f,
+                                22.5f,
+                                25.0f,
+                                27.5f,
+                                30.0f,
+                                32.5f,
+                                35.0f,
+                                37.5f ) );
 }
 
 TEST_CASE( "LinearInterpolation_Vec2f" )
 {
-    gm::Vec2f vectorA( 0.0f, 2.0f );
-    gm::Vec2f vectorB( 0.0f, 4.0f );
-    gm::Vec2f vectorLerp = gm::LinearInterpolation( vectorA, vectorB, 0.5f );
-    CHECK( vectorLerp[ 0 ] == Approx( 0.0f ) );
-    CHECK( vectorLerp[ 1 ] == Approx( 3.0f ) );
+    gm::Vec2f source = gm::Vec2f( 0.0f, 0.0f );
+    gm::Vec2f target = gm::Vec2f( 0.0f, 5.0f );
+    gm::Vec2f result = gm::LinearInterpolation( source,
+                                                target,
+                                                /* weight */ 0.5 );
+
+    CHECK( result == gm::Vec2f( 0.0f, 2.5f ) );
 }
 
 TEST_CASE( "LinearInterpolation_Vec3f" )
 {
-    gm::Vec3f vectorA( 0.0f, 2.0f, 4.0f );
-    gm::Vec3f vectorB( 0.0f, 4.0f, 8.0f );
-    gm::Vec3f vectorLerp = gm::LinearInterpolation( vectorA, vectorB, 0.5f );
-    CHECK( vectorLerp[ 0 ] == Approx( 0.0f ) );
-    CHECK( vectorLerp[ 1 ] == Approx( 3.0f ) );
-    CHECK( vectorLerp[ 2 ] == Approx( 6.0f ) );
+    gm::Vec3f source = gm::Vec3f( 0.0f, 0.0f, 0.0f );
+    gm::Vec3f target = gm::Vec3f( 0.0f, 5.0f, 10.0f );
+    gm::Vec3f result = gm::LinearInterpolation( source,
+                                                target,
+                                                /* weight */ 0.5 );
+
+    CHECK( result == gm::Vec3f( 0.0f, 2.5f, 5.0f ) );
 }
 
 TEST_CASE( "LinearInterpolation_Vec4f" )
 {
-    gm::Vec4f vectorA( 0.0f, 2.0f, 4.0f, 6.0f );
-    gm::Vec4f vectorB( 0.0f, 4.0f, 8.0f, 12.0f );
-    gm::Vec4f vectorLerp = gm::LinearInterpolation( vectorA, vectorB, 0.5f );
-    CHECK( vectorLerp[ 0 ] == Approx( 0.0f ) );
-    CHECK( vectorLerp[ 1 ] == Approx( 3.0f ) );
-    CHECK( vectorLerp[ 2 ] == Approx( 6.0f ) );
-    CHECK( vectorLerp[ 3 ] == Approx( 9.0f ) );
+    gm::Vec4f source = gm::Vec4f( 0.0f, 0.0f, 0.0f, 0.0f );
+    gm::Vec4f target = gm::Vec4f( 0.0f, 5.0f, 10.0f, 15.0f );
+    gm::Vec4f result = gm::LinearInterpolation( source,
+                                                target,
+                                                /* weight */ 0.5 );
+
+    CHECK( result == gm::Vec4f( 0.0f, 2.5f, 5.0f, 7.5f ) );
 }
--- src/gm/template/functions/tests/testLinearInterpolation.cpp
+++ src/gm/template/functions/tests/testLinearInterpolation.cpp
@@ -2,51 +2,21 @@
 
 #include <gm/functions/{{ function.headerFileName }}>
 
+{% import "types/typeUtils.h" as typeUtils %}
+
 {% for interface in function.interfaces %}
+{% set valueType            = interface.ArgType("source") %}
+{% set namespacedValueClass = valueType.namespacedClassName %}
 TEST_CASE( "{{ function.name }}_{{ interface.ArgClass("source") }}" )
 {
-{%- if interface.Arg("source").type.isScalar -%}
-    {{ interface.ArgClass("source") }} {{ interface.ArgType("source").varName }}A =
-        {{ interface.Arg("source").type.CppValue(3) }};
-    {{ interface.ArgClass("source") }} {{ interface.ArgType("source").varName }}B =
-        {{ interface.Arg("source").type.CppValue(1) }};
-{%- elif interface.Arg("source").type.isVector -%}
-    gm::{{ interface.ArgClass("source") }} {{ interface.ArgType("source").varName }}A(
-{% for index in range(interface.Arg("source").type.elementSize) -%}
-    {{ interface.Arg("source").type.CppValue(index * 2) }}
-{%- if index + 1 < interface.Arg("source").type.elementSize -%}
-        ,
-{%- endif -%}
-{%- endfor %}
+    {{ namespacedValueClass }} source = {{ typeUtils.GenArithmeticSequence(valueType, 0) }};
+    {{ namespacedValueClass }} target = {{ typeUtils.GenArithmeticSequence(valueType, 5) }};
+    {{ namespacedValueClass }} result = gm::{{ function.name }}(
+        source,
+        target,
+        /* weight */ 0.5
     );
-    gm::{{ interface.ArgClass("target") }} {{ interface.ArgType("target").varName }}B(
-{% for index in range(interface.Arg("target").type.elementSize) -%}
-    {{ interface.Arg("target").type.CppValue(index * 4) }}
-{%- if index + 1 < interface.Arg("target").type.elementSize -%}
-        ,
-{%- endif -%}
-{%- endfor %}
-    );
-{%- endif %}
 
-{%- if not interface.Arg("source").type.isScalar -%}
-    gm::
-{%- endif -%}
-    {{ interface.ArgClass("source") }} {{ interface.ArgType("source").varName }}Lerp =
-        gm::{{ function.name }}(
-            {{ interface.ArgType("source").varName }}A,
-            {{ interface.ArgType("target").varName }}B,
-            {{ interface.ArgType("target").CppValue(0.5) }}
-        );
-
-{%- if interface.Arg("source").type.isScalar -%}
-    CHECK( {{ interface.ArgType("source").varName }}Lerp
-           == Approx( {{ interface.Arg("source").type.CppValue(2) }} ));
-{%- elif interface.Arg("source").type.isVector -%}
-{% for index in range(interface.Arg("source").type.elementSize) -%}
-    CHECK( {{ interface.ArgType("source").varName }}Lerp[ {{ index }} ]
-           == Approx( {{ interface.Arg("source").type.CppValue( index * 3 ) }} ));
-{%- endfor %}
-{%- endif %}
+    CHECK( result == {{ typeUtils.GenArithmeticSequence(valueType, 2.5) }} );
 }
 {% endfor %}
